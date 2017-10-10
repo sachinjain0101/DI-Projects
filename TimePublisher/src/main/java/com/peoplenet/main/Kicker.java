f355161a0dcedd8c5a7bf7a4c303fca4098de1a6
sachinjain0101 <sachinjain.0101@gmail.com>
2017-10-10 16:12:59 -0400

    Initial Commit

--- TimePublisher/src/main/java/com/peoplenet/main/Kicker.java
+++ TimePublisher/src/main/java/com/peoplenet/main/Kicker.java
@@ -1,10 +1,10 @@
 package com.peoplenet.main;
 
-import com.peoplenet.ws.v1.RestTest;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.servlet.ServletContainer;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,25 +12,21 @@ public class Kicker {
     public static void main(String[] args) {
         Server server = new Server(7676);
 
-        ServletContextHandler ctx =
-                new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+        ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
 
         ctx.setContextPath("/");
         server.setHandler(ctx);
 
-        ServletHolder serHol = ctx.addServlet(ServletContainer.class, "/rest/*");
-        serHol.setInitOrder(1);
-        serHol.setInitParameter("jersey.config.server.provider.packages",
-                "com.peoplenet.ws.v1");
+        ServletHolder servHolder = ctx.addServlet(ServletContainer.class, "/*");
+        servHolder.setInitOrder(1);
+        servHolder.setInitParameter("jersey.config.server.provider.packages","com.peoplenet.ws.v1");
 
         try {
             server.start();
             server.join();
         } catch (Exception ex) {
-
             Logger.getLogger(Kicker.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
-
             server.destroy();
         }
     }
